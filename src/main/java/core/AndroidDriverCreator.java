35a8c9f244b76cfa703a339fff824747f965e905
Aliaksandr_Urbanovic <urbanovichalexandr@mail.ru>
2018-08-15 17:45:14 +0300

    Initial commit

---
+++ src/main/java/core/AndroidDriverCreator.java
@@ -0,0 +1,9 @@
+package core;
+
+import org.apache.log4j.Logger;
+
+public class AndroidDriverCreator {
+    private static final Logger log = Logger.getLogger(AndroidDriverCreator.class);
+
+    
+}
---
+++ src/main/java/weblayout/pages/BasePage.java
@@ -0,0 +1,9 @@
+package weblayout.pages;
+
+import ru.yandex.qatools.htmlelements.loader.HtmlElementLoader;
+
+public abstract class BasePage {
+    protected BasePage() {
+//        HtmlElementLoader.populatePageObject(this, WebDriverManager.getWDriver());
+    }
+}
