9bb9035446aeb03e0cb825bc59fd6472448f0981
Aliaksandr_Urbanovic <urbanovichalexandr@mail.ru>
2018-08-16 15:08:16 +0300

    16/08/2018

---
+++ src/main/java/weblayout/elements/CLabel.java
@@ -0,0 +1,11 @@
+package weblayout.elements;
+
+import org.openqa.selenium.WebElement;
+
+public class CLabel extends CustomElement {
+
+    public CLabel(WebElement wrappedElement) {
+        super(wrappedElement);
+    }
+
+}
---
+++ src/main/java/weblayout/pages/SettingsPage.java
@@ -0,0 +1,22 @@
+package weblayout.pages;
+
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import weblayout.elements.CLabel;
+
+public class SettingsPage {
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[5]")
+    private CLabel wifi;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[9]")
+    private CLabel display;
+
+
+    public CLabel getWifi() {
+        return wifi;
+    }
+
+    public CLabel getDisplay() {
+        return display;
+    }
+}
