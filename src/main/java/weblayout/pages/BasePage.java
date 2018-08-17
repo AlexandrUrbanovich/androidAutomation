d8940519e85247e04404f51187b2526fed29fd5d
Aliaksandr_Urbanovic <urbanovichalexandr@mail.ru>
2018-08-17 17:24:00 +0300

    17/08/2018

--- src/main/java/weblayout/pages/BasePage.java
+++ src/main/java/weblayout/pages/BasePage.java
@@ -1,16 +1,26 @@
 package weblayout.pages;
 
 import core.AndroidDriverCreator;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
 import ru.yandex.qatools.htmlelements.loader.HtmlElementLoader;
 
 import java.net.MalformedURLException;
 
 public abstract class BasePage {
     protected BasePage() {
+//        try {
+//            HtmlElementLoader.populatePageObject(this, AndroidDriverCreator.getDriver());
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
         try {
-            HtmlElementLoader.populatePageObject(this, AndroidDriverCreator.getDriver());
+            PageFactory.initElements(new AppiumFieldDecorator(AndroidDriverCreator.getDriver()), this);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+
     }
 }
