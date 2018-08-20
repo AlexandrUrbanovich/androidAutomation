9e3f78d07a21428e6f65b8de187011068486bef9
Aliaksandr_Urbanovic <urbanovichalexandr@mail.ru>
2018-08-20 10:45:56 +0300

    20/08/2018

--- src/main/java/weblayout/pages/SettingPage.java
+++ src/main/java/weblayout/pages/SettingPage.java
@@ -16,6 +16,27 @@ public class SettingPage extends BasePage {
     @WithTimeout(time = 20, chronoUnit = ChronoUnit.SECONDS)
     private WebElement display;
 
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[1]")
+    private WebElement notifications;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[2]")
+    private WebElement sound;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[3]")
+    private WebElement apps;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[4]")
+    private WebElement storage;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[5]")
+    private WebElement battery;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[6]")
+    private WebElement memory;
+
+    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[7]")
+    private WebElement users;
+
     public WebElement getWifi() {
         return wifi;
     }
@@ -24,5 +45,31 @@ public class SettingPage extends BasePage {
         return display;
     }
 
+    public WebElement getNotifications() {
+        return notifications;
+    }
 
+    public WebElement getSound() {
+        return sound;
+    }
+
+    public WebElement getApps() {
+        return apps;
+    }
+
+    public WebElement getStorage() {
+        return storage;
+    }
+
+    public WebElement getBattery() {
+        return battery;
+    }
+
+    public WebElement getMemory() {
+        return memory;
+    }
+
+    public WebElement getUsers() {
+        return users;
+    }
 }
