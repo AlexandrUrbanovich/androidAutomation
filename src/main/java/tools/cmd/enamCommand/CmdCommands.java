fbd8a7b14605629a414b918f32b67020566558c5
Aliaksandr_Urbanovic <urbanovichalexandr@mail.ru>
2018-08-23 16:00:36 +0300

    23/08/2018

--- src/main/java/tools/cmd/enamCommand/CmdCommands.java
+++ src/main/java/tools/cmd/enamCommand/CmdCommands.java
@@ -3,7 +3,8 @@ package tools.cmd.enamCommand;
 public enum CmdCommands {
     REBOOT("adb -s emulator-5554 reboot"),
     DELETE_ALL_DATE("adb shell pm clear com.android.settings"),
-    DEVICE("adb device");
+    DEVICE("adb device"),
+    SHELL("adb shell");
 
     public String name;
 
