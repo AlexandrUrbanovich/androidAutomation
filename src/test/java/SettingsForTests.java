4c20761f256b1e192b552276a8630d2c4c732979
Aliaksandr_Urbanovic <urbanovichalexandr@mail.ru>
2018-08-30 17:00:39 +0300

    30/08/2018

--- src/test/java/SettingsForTests.java
+++ src/test/java/SettingsForTests.java
@@ -3,14 +3,11 @@ import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 import services.modules.CustomFrameworkModule;
 import tools.appiumServer.AppiumServerJava;
-import tools.cmd.enamCommand.CmdCommands;
 
 @Guice(modules = {CustomFrameworkModule.class})
-
 public class SettingsForTests {
     private static final Logger log = Logger.getLogger(SettingsForTests.class);
     public static SoftAssert softAssert;
-//    private String cmdCommand = CmdCommands.REBOOT.getCommand();
 
     AppiumServerJava appiumServerJava = new AppiumServerJava();
 
@@ -22,35 +19,17 @@ public class SettingsForTests {
 
     @BeforeMethod
     public void beforeMethod() {
-
-//        appiumServerJava.startServer();
-//        log.info("Server RUN!!!");
         softAssert = new SoftAssert();
         log.info("SoftAssert was created");
     }
 
     @AfterMethod
-    public void afterMethod() {
-//        appiumServerJava.stopServer();
-//        log.info("Server STOP!!!");
-
-//        CmdManager.cmdCommand(cmdCommand);
-//        try {
-//            Thread.sleep(30000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-    }
-
-//    @AfterTest
-//    public void delCookie() {
-//        log.info("Cookie was cleaned");
-//        AndroidDriverCreator.getDriver().manage().deleteAllCookies();
-//    }
+    public void afterMethod() {}
 
     @AfterSuite
     public void afterSuit() {
         appiumServerJava.stopServer();
         log.info("Server STOP!!!");
     }
+
 }
